aa3b75a729c25fc0d6d54cdaa89e8d40c1eff2e5
Folkvich <Folkvichaya333@gmail.com>
2022-11-24 16:29:47 +0700

    backend

---
+++ main.ts
@@ -0,0 +1,124 @@
+import * as express from 'express';
+import * as mysql from 'mysql2'
+import * as cors from 'cors'
+const app = express()
+
+app.use(cors())
+
+const getSQLConnection = (errorResponse: express.Response): Promise<mysql.PoolConnection> => {
+    return new Promise((resolve, reject) => {
+        mysql.createPool({
+            user: "root",
+            password: "",
+            host: "127.0.0.1",
+            port: 3306,
+            database: "seven",
+            namedPlaceholders: true
+        }).getConnection((err, conn) => {
+            if (err) {
+                errorResponse.json({ status: "not ok", error: err })
+                return resolve(null);
+            }
+            return resolve(conn)
+        })
+    })
+}
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/getstore', async (req, res) => {
+    const sql = `SELECT * FROM store WHERE store_id = ${req.query.id}`
+    console.log(req.query);
+    const conn = await getSQLConnection(res)
+    if(!conn) return;
+    conn.execute(sql, (err, result: any[]) => {
+        if (err) return res.status(400).json({ status: "not ok", error: err });
+        if (result.length == 0) return res.status(400).json({ status: "not found" });
+        return res.status(200).json({ status: "ok", result: result })
+    })
+})
+
+// app.post('/login', async (req, res) => {
+//     const body = req.body ?? {}
+//     const conn = await getSQLConnection(res)
+//     if(!conn) return;
+//     const loginSQL = `SELECT * FROM stroe`;
+//     conn.execute(loginSQL, {
+//         user: body.username ?? "",
+//         pass: body.password ?? "",
+//     }, (err, result: any[]) => {
+//         if (err) return res.status(400).json({ status: "not ok", error: err });
+//         if (result.length == 0) return res.status(400).json({ status: "not found: user or password wrong" });
+//         return res.status(200).json({ status: "ok", result: result[0] })
+//     })
+// })
+
+// app.get('/getSuggestList', async (req, res) => {
+//     const isfull = req.query.full == "true"
+//     const requester = req.query.requester;
+
+//     if (!requester) return res.status(400).json({ status: "not ok, id not found" });
+
+//     const sql = isfull ? `SELECT s.*, concat(u.fname, " ", u.lname) as requester_name 
+//     ,t.label as topic_label
+//     FROM suggest s 
+//     left join user u on u.id = s.requester
+//     left join topic t on s.topic_id = t.id
+//     ` : 
+//     `SELECT s.*, concat(u.fname, " ", u.lname) as requester_name 
+//     ,t.label as topic_label
+//     FROM suggest s 
+//     left join user u on u.id = s.requester 
+//     left join topic t on s.topic_id = t.id
+//     where requester = :requester`;
+//     const conn = await getSQLConnection(res)
+//     if(!conn) return;
+//     conn.execute(sql, { requester }, (err, result: any[]) => {
+//         if (err) return res.status(400).json({ status: "not ok", error: err });
+//         if (result.length == 0) return res.status(400).json({ status: "not found" });
+//         return res.status(200).json({ status: "ok", result: result })
+//     })
+// })
+
+// app.post('/postSuggest', async (req, res) => {
+//     const requester = req.body.requester;
+//     const topic = req.body.topic;
+//     const text = req.body.text;
+//     const type = req.body.type;
+
+//     if (!requester || !topic || !text) return res.status(400).json({ status: "not ok, invalid param" });
+
+//     // 0 = เปิดเผย
+//     // 1 = ส่วนตัว
+
+//     const sql = `insert into suggest (requester, approver, topic_id, suggestion, create_date, update_date, suggest_type, complete) value (:requester, null, :topic, :text,  now(), now(), :type, 0)`;
+//     const conn = await getSQLConnection(res)
+//     if(!conn) return;
+//     conn.execute(sql, { requester, topic, text, type }, (err, result) => {
+//         if (err) return res.status(400).json({ status: "not ok", error: err });
+//         // if (result.length == 0) return res.status(400).json({ status: "not found" });
+//         return res.status(200).json({ status: "ok", result: result })
+//     })
+// })
+
+// app.post('/confirm', async (req, res) => {
+//     const approver = req.body.approver;
+//     const suggest = req.body.suggest;
+
+//     if (!approver || !suggest) return res.status(400).json({ status: "not ok, invalid param" });
+
+//     const sql = `update suggest set complete = 1, approver = :approver where id = :id`;
+//     const conn = await getSQLConnection(res)
+//     if(!conn) return;
+//     conn.execute(sql, { id: suggest, approver }, (err, result: any) => {
+//         if (err) return res.status(400).json({ status: "not ok", error: err });
+//         if (result.affectedRows == 0) return res.status(400).json({ status: "error, no change" });
+//         return res.status(200).json({ status: "ok", result: result })
+//     })
+// })
+
+app.listen(3300, () => {
+    console.log('Start server at port 3300.')
+})
